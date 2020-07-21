95c09df58f0d54e3c3de0aa637cb04129aff31cb
nyamka <unyamka@gmail.com>
2020-07-21 17:40:27 +0900

    update

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,49 +1,65 @@
-import React, {useState} from 'react';
-import {View, Button, Platform} from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Platform, TextInput, StyleSheet } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 export const DatePicker = () => {
-  const [date, setDate] = useState(new Date(1598051730000));
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setShow(Platform.OS === 'ios');
-    setDate(currentDate);
-  };
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+    };
 
-  const showMode = currentMode => {
-    setShow(true);
-    setMode(currentMode);
-  };
+    const showMode = currentMode => {
+        setShow(true);
+        setMode(currentMode);
+        alert("asdf");
+    };
 
-  const showDatepicker = () => {
-    showMode('date');
-  };
+    const showDatepicker = () => {
+        showMode('date');
+    };
 
-  const showTimepicker = () => {
-    showMode('time');
-  };
+    const showTimepicker = () => {
+        showMode('time');
+    };
 
-  return (
-    <View>
-      <View>
-        <Button onPress={showDatepicker} title="Show date picker!" />
-      </View>
-      <View>
-        <Button onPress={showTimepicker} title="Show time picker!" />
-      </View>
-      {show && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode={mode}
-          is24Hour={true}
-          display="default"
-          onChange={onChange}
-        />
-      )}
-    </View>
-  );
+    return (
+        <View style={{ width: "90%", backgroundColor: "green"}}>
+            <View>
+                <TextInput style={styles.textInput} onClick={showDatepicker} />
+            </View>
+            <View>
+                <Button onPress={showTimepicker} title="Show time picker!" />
+            </View>
+            {show && (
+                <DateTimePicker
+                    style ={{ }}
+                    testID="dateTimePicker"
+                    value={date}
+                    mode={mode}
+                    is24Hour={true}
+                    display="default"
+                    onChange={onChange}
+                    textColor="black"
+                />
+            )}
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    textInput: {
+        borderRadius: 5,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderColor: "red",
+        borderWidth: 1,
+        color: "black",
+        backgroundColor: "white",
+        height:40
+    }
+});
